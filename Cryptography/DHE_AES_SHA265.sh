5addf2436c0b457a462c03c357d236ef8ebbbd4b
Cloud User <centos@ip-10-10-0-132.eu-west-1.compute.internal>
2021-09-16 13:40:45 +0000

    Improved flow

--- Cryptography/DHE_AES_SHA265.sh
+++ Cryptography/DHE_AES_SHA265.sh
@@ -1,52 +1,105 @@
 function pause(){
-	read -p "$*"
+	echo " "
+	read -p "press enter key to proceed"
+	echo " "
 }
+
+function lf(){
+	echo "Files: "; ls 
+}
+
+if [[ $1 == 'clean' ]]
+then
+	echo "Cleaning files"
+	rm cipher-sent.bin dhkey_Alice.pem dhkey_Bob.pem dhp.pem dhpub_Alice.pem dhpub_Bob.pem secret_alice.bin secret_bob.bin plain-received.txt
+	exit 1
+fi
 
 reset
 echo "=================================================================== "
-echo "This script emulates an ephemeral Diffie-Hellam (EDH) key exchange "
+echo "This script emulates an ephemeral Diffie-Hellman (EDH) key exchange "
 echo "agreement between Alice and Bob. The exchange agreement is ephemeral "
-echo "as the public key exchange paramters never get re-used."
+echo "as the public key exchange parameters never get re-used."
 echo "=================================================================== "
 
-echo " "
-pause 'press enter to proceed'
-echo " "
+pause
 
+echo "==DH== (1) Key Exchange"
+echo " "
+echo "==DH== 1.i"
 echo "==DH== Generate DH initial parameters p (large prime) and g (the generator)"
-	openssl dhparam -out dhp.pem 512 2>/dev/null
-	openssl pkeyparam -in dhp.pem -text -noout
+echo "==DH== openssl dhparam -out dhp.pem 512"
+openssl dhparam -out dhp.pem 512 2>/dev/null
 
-echo "==DH== Use the DH initial paramters to generate Bob's private key: g (generator), p (prime), b (private exponent), g^b mod(p) (public point) "
-	openssl genpkey -paramfile dhp.pem -out dhkey_Bob.pem > /dev/null
-  	openssl pkey -in dhkey_Bob.pem -text -noout
+echo "==DH== openssl pkeyparam -in dhp.pem -text -noout"
+openssl pkeyparam -in dhp.pem -text -noout
+lf
 
+pause
+
+echo "==DH== 1.ii"
+echo "==DH== Use the DH initial parameters to generate Bob's private key: g (generator), p (prime), b (private exponent), g^b mod(p) (public point) "
+echo "==DH== openssl genpkey -paramfile dhp.pem -out dhkey_Bob.pem"
+openssl genpkey -paramfile dhp.pem -out dhkey_Bob.pem 2>/dev/null
+
+echo "==DH== openssl pkey -in dhkey_Bob.pem -text -noout"
+openssl pkey -in dhkey_Bob.pem -text -noout
+lf
+
+pause
+
+echo "==DH== 1.iii"
 echo "==DH== Extract public information from Bob's private key (dhkey_Bob.pem) to produce
       public key: g (generator), p (prime), g^b mod(p) (Bob's public point)"
-  	openssl pkey -in dhkey_Bob.pem -pubout -out  dhpub_Bob.pem
-  	openssl  pkey -pubin -in dhpub_Bob.pem  -text -noout
+echo "==DH== openssl pkey -in dhkey_Bob.pem -pubout -out  dhpub_Bob.pem"
+openssl pkey -in dhkey_Bob.pem -pubout -out  dhpub_Bob.pem
 
+echo "==DH== openssl pkey -pubin -in dhpub_Bob.pem  -text -noout"
+openssl pkey -pubin -in dhpub_Bob.pem  -text -noout
+lf
 
+pause
+
+echo "==DH== 1.iv"
 echo "==DH== Use the DH initial paramters to generate Alices's 
       private key: g (generator), p (prime), a (private exponent), g^a mod(p) (public point) "
-	openssl genpkey -paramfile dhp.pem -out dhkey_Alice.pem
+	
+echo "==DH== openssl genpkey -paramfile dhp.pem -out dhkey_Alice.pem"
+openssl genpkey -paramfile dhp.pem -out dhkey_Alice.pem
+lf
 
+pause
+
+echo "==DH== 1.v"
 echo "==DH== Extract public information from Alice's private key (dhkey_Alice.pem) to produce
       public key: g (generator), p (prime), g^a mod(p) (Alice's public point)"
- 	openssl pkey -in dhkey_Alice.pem -pubout -out  dhpub_Alice.pem
-	openssl  pkey -pubin -in dhpub_Alice.pem  -text -noout
+echo "==DH== openssl pkey -in dhkey_Alice.pem -pubout -out  dhpub_Alice.pem"
+openssl pkey -in dhkey_Alice.pem -pubout -out  dhpub_Alice.pem
 
-echo " "
-pause 'press enter to proceed'
-echo " "
+echo "==DH== openssl  pkey -pubin -in dhpub_Alice.pem  -text -noout"
+openssl  pkey -pubin -in dhpub_Alice.pem  -text -noout
+lf
 
+pause
+
+echo "==DH== 1.vi"
 echo "==DH== Alice derives common secret (secret_alice.bin) using their private key (dhkey_Alive) and Bob's peer key (dhpub_Bob)"
-	openssl pkeyutl -derive -keyform PEM -inkey dhkey_Alice.pem -peerkey dhpub_Bob.pem -out secret_alice.bin
+echo "==DH== openssl pkeyutl -derive -keyform PEM -inkey dhkey_Alice.pem -peerkey dhpub_Bob.pem -out secret_alice.bin"
+openssl pkeyutl -derive -keyform PEM -inkey dhkey_Alice.pem -peerkey dhpub_Bob.pem -out secret_alice.bin
+lf
 
+pause
   
-	echo "==DH== Bob independently derives common secret (secret_bob.bin) using their private key (dhkey_Bob) and Alices's peer key (dhpub_Alice)"
-	openssl pkeyutl -derive -keyform PEM -inkey dhkey_Bob.pem -peerkey dhpub_Alice.pem -out secret_bob.bin
+echo "==DH== 1.vii"
+echo "==DH== Bob independently derives common secret (secret_bob.bin) using their private key (dhkey_Bob) and Alices's peer key (dhpub_Alice)"
+echo "==DH== openssl pkeyutl -derive -keyform PEM -inkey dhkey_Bob.pem -peerkey dhpub_Alice.pem -out secret_bob.bin"
+openssl pkeyutl -derive -keyform PEM -inkey dhkey_Bob.pem -peerkey dhpub_Alice.pem -out secret_bob.bin
+lf 
 
+pause
+
+echo "==ENC== (2) Encryption"
+echo " "
 echo "=========================================================================================================="
 echo "At this stage, secret_alice.bin and secret_bob.bin should be identical symmmetric keys:"
 	sha256sum secret_*
@@ -59,38 +112,62 @@ echo "4 - Bob follows the protocol and first checks the integrity of the cipher 
 echo "5 - if message integrity passes, Bob decrypts cipher-sent.bin into plain-received.txt using secret_bob.bin."
 echo "=========================================================================================================="
 
-echo " "
-pause 'press enter to proceed'
-echo " "
+pause
 
-# We need to represent 16 random characters (16 bytes) into hex (0xXX):
+echo "==ENC== We need to represent 16 random characters (16 bytes) into hex (0xXX)"
 echo "==ENC== Public information: we are generating a 128bit IV for the current encryption:"
-	IV=$(hexdump -n 16 -e '"%X"'  /dev/urandom )
-	echo $IV
-echo ""
+IV=$(hexdump -n 16 -e '"%X"'  /dev/urandom )
+echo $IV
+
 echo "==ENC== Alice encrypts \"plain.txt\", using the advertised IV, to produce \"cipher-sent.bin\" "
-	openssl enc -e -iv $(echo $IV) -aes-256-cbc \
+echo "==ENC== openssl enc -e -iv \$(echo \$IV) -aes-256-cbc \ "
+echo "==ENC==		-K \$(hexdump -v -e '/1 "%02X"' secret_alice.bin) \ "
+echo "==ENC==		-in plain.txt -out cipher-sent.bin"
+
+openssl enc -e -iv $(echo $IV) -aes-256-cbc \
 		-K $(hexdump -v -e '/1 "%02X"' secret_alice.bin) \
 		-in plain.txt -out cipher-sent.bin 2>/dev/null
+
+lf
+pause
+
+echo "==TAG== (3) Message Authentication Code"
+echo " "
+echo "==TAG== 3.i"
 echo "==TAG== Public information: Alice sends basic integrity checks to Bob which they have agreed should be checked before proceeding with decryting received messages:"
-	TAG=$((sha256sum cipher-sent.bin; echo ALICE) | sha256sum )
+echo "==TAG== TAG=$((sha256sum cipher-sent.bin; echo ALICE) | sha256sum )"
+TAG=$((sha256sum cipher-sent.bin; echo ALICE) | sha256sum )
+
 echo "==TAG== The chosen TAG (SHA256(SHA256 CIPHER | ALICE) is  $TAG"
-# Now Bob will decrypt this message with their derived key, 
-# but ONLY if the sent tag is the same as the one they calculate.
+
+pause 
+
+echo "==TAG== 3.ii"
+echo "==TAG== Now Bob will decrypt this message with their derived key, "
+echo "==TAG== but ONLY if the sent tag is the same as the one they calculate."
 echo "==TAG== Bob evaluates message TAG"
-	TAG2=$((sha256sum cipher-sent.bin; echo ALICIA) | sha256sum) 
-echo "==TAG== Received TAG avaluated by Bob is: $TAG2"
-	if [[ "$TAG2" == "$TAG" ]]; then 
-		echo "==DEC== Message TAG test: PASSED ..."
-		echo "==DEC== Bob decrypts \"cipher-sent.bin\", using the advertised IV, to produce \"plain-received.txt\" "
-		openssl enc -d -iv $(echo $IV) -aes-256-cbc \
-		-K $(hexdump -v -e '/1 "%02X"' secret_bob.bin) \
-		-in cipher-sent.bin -out plain-received.bin 2>/dev/null
-		cat plain-received.bin
-	else
-		echo "==ERROR== Message TAG test failed. Message integrity is not preserved. Will not decrypt."
-		exit -1
-	fi
+echo "==TAG== TAG2=$((sha256sum cipher-sent.bin; echo ALICIA) | sha256sum) "
+TAG2=$((sha256sum cipher-sent.bin; echo ALICIA) | sha256sum) 
+
+
+echo "==TAG== Received TAG evaluated by Bob is: $TAG2"
+
+pause
+
+echo "==TAG== Testing tags:"
+if [[ "$TAG2" == "$TAG" ]]; then 
+	echo "==DEC== (4) Decryption"
+	echo " "
+	echo "==DEC== Message TAG test: PASSED ..."
+	echo "==DEC== Bob decrypts \"cipher-sent.bin\", using the advertised IV, to produce \"plain-received.txt\" "
+	openssl enc -d -iv $(echo $IV) -aes-256-cbc \
+	-K $(hexdump -v -e '/1 "%02X"' secret_bob.bin) \
+	-in cipher-sent.bin -out plain-received.txt 2>/dev/null
+	cat plain-received.txt
+else
+	echo "==ERROR== Message TAG test failed. Message integrity is not preserved. Will not decrypt."
+	exit -1
+fi
 echo "Done!"
 
 
