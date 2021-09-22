6382096aff2991b4a9f1a34042b9a9000a8b231c
Cloud User <centos@ip-10-10-0-132.eu-west-1.compute.internal>
2021-09-22 06:34:04 +0000

    added output file as parameter

--- FilesystemIntegrityAndUpdates/generatetestfile.sh
+++ FilesystemIntegrityAndUpdates/generatetestfile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # generate 10MB testfile containing printable characters only
-< /dev/urandom tr -dc '[:graph:]' | head -c 10M > ./testfile
+< /dev/urandom tr -dc '[:graph:]' | head -c 10M > $1
